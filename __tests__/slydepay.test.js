a88212037b82fddf856b8c3ce6a6707b13d4e749
David Oddoye <oddoyedavid@gmail.com>
2017-09-05 23:03:23 +0000

    stub functions and more tests

--- __tests__/slydepay.test.js
+++ __tests__/slydepay.test.js
@@ -1,1 +1,38 @@
-import Slydepay from '../';
+import Slydepay from '../lib/slydepay';
+
+describe('Instantiation Tests', () => {
+  test('Should initialize a SlydePay instance given emailOrMobileNumber and merchantKey', () => {
+    const instance = Slydepay.init({
+      emailOrMobileNumber: 'merchant@awesomecustomer.com',
+      merchantKey: 'thatkeyyoushouldkeepsecret',
+    });
+
+    expect(instance).toBeTruthy();
+  });
+
+  test('Should not initialize a SlydePay instance if emailOrMobileNumber or merchantKey is not provided', () => {
+    expect(() => {
+      Slydepay.init({
+        merchantKey: 'thatkeyyoushouldkeepsecret',
+      });
+    }).toThrowError();
+  });
+
+  test('Should not initialize a SlydePay instance if emailOrMobileNumber is not an email or number', () => {
+    expect(() => {
+      Slydepay.init({
+        emailOrMobileNumber: 'foo',
+        merchantKey: 'thatkeyyoushouldkeepsecret',
+      });
+    }).toThrowError();
+  });
+
+  test('Should not initialize a SlydePay instance if merchantKey is not a string', () => {
+    expect(() => {
+      Slydepay.init({
+        emailOrMobileNumber: 'merchant@awesomecustomer.com',
+        merchantKey: 1234,
+      });
+    }).toThrowError();
+  });
+});
